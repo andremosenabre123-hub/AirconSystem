f989fd7e05b9a780a7cade159574ef68d5857de0
andremosenabre123-hub <andremosenabre123@gmail.com>
2026-09-16 16:39:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const nameInput = document.getElementById("nameInput");
+const sendButton = document.getElementById("sendButton");
+const message = document.getElementById("message");
+
+
+const API_URL = "http://127.0.0.1:8000";
+
+
+sendButton.addEventListener("click", sendName);
+
+
+async function sendName() {
+
+    const name = nameInput.value.trim();
+
+
+    if (name === "") {
+
+        message.textContent = "Please enter your name.";
+
+        return;
+    }
+
+
+    try {
+
+        const response = await fetch(`${API_URL}/names`, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                name: name
+            })
+
+        });
+
+
+        const data = await response.json();
+
+
+        if (!response.ok) {
+
+            message.textContent = "Failed to save name.";
+
+            return;
+        }
+
+
+        message.textContent = data.message;
+
+        nameInput.value = "";
+
+
+    } catch (error) {
+
+        console.error(error);
+
+        message.textContent = "Could not connect to API.";
+
+    }
+}
